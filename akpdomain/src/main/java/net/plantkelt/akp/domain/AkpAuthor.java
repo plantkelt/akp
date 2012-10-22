c46ec02fc57d16e279884fef71d461cd9d845f9d
laurent <devnull@localhost>
2012-10-22 19:14:57 +0200

    Add author mapping and object model.

---
+++ akpdomain/src/main/java/net/plantkelt/akp/domain/AkpAuthor.java
@@ -0,0 +1,47 @@
+package net.plantkelt.akp.domain;
+
+public class AkpAuthor implements Comparable<AkpAuthor> {
+
+	private String xid;
+	private String name;
+	private String source;
+	private String dates;
+
+	public String getXid() {
+		return xid;
+	}
+
+	public void setXid(String xid) {
+		this.xid = xid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getDates() {
+		return dates;
+	}
+
+	public void setDates(String dates) {
+		this.dates = dates;
+	}
+
+	@Override
+	public int compareTo(AkpAuthor o) {
+		return getXid().compareTo(o.getXid());
+	}
+
+}
