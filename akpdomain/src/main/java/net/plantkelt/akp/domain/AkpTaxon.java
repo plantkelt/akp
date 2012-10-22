5a4a03247399cd8ba6d007e4360e021827aeb16a
laurent <devnull@localhost>
2012-10-22 19:15:46 +0200

    Epsilon-related stuff in taxon (html rendering, ordering).

--- akpdomain/src/main/java/net/plantkelt/akp/domain/AkpTaxon.java
+++ akpdomain/src/main/java/net/plantkelt/akp/domain/AkpTaxon.java
@@ -5,12 +5,34 @@ public class AkpTaxon implements Comparable<AkpTaxon> {
 	public static final int TYPE_MAIN = 0;
 	public static final int TYPE_SYNONYM = 2;
 
+	private static final String[] EPSILON_LIST = { "sect.", "subsect.",
+			"subsp.", "[subsp.]", "nothosubsp.", "proles", "nothoproles",
+			"var.", "[var.]", "nothovar.", "subvar.", "subgen.", "fa.",
+			"subfa.", "convar.", "cv.", "infrasp." };
+	private static final String[] EPSILON2_LIST = { " non ", " nec ", " ex ",
+			" & ", " an ", " excl. ", " incl. ", " emend. ", " fide ", " vel ",
+			" ser. ", " apud ", " lusus ", "<i>orth. var.</i>", "<i>p. p.</i>",
+			"<i>p. max. p.</i>", "<i>p. min. p.</i>", "<i>lapsus calami ?</i>" };
+	private static final String[] EPSILON3_LIST = { "<i>sensu</i>",
+			"<i>pro sp.</i>", "<i>pro hybr.</i>" };
+
+	private static final String[] escapedEpsilonList;
+
+	static {
+		escapedEpsilonList = new String[EPSILON_LIST.length];
+		for (int i = 0; i < EPSILON_LIST.length; i++) {
+			escapedEpsilonList[i] = EPSILON_LIST[i].replace(".", "\\.")
+					.replace("[", "\\[").replace("]", "\\]");
+		}
+	}
+
 	private Integer xid;
 	private AkpPlant plant;
 	private String name;
 	private int type;
 
 	private transient String sortKey;
+	private transient String xName;
 
 	public Integer getXid() {
 		return xid;
@@ -33,22 +55,42 @@ public class AkpTaxon implements Comparable<AkpTaxon> {
 	}
 
 	public String getHtmlName() {
-		// TODO compute and cache the returned value
-		String html = name.replace("<x>", "×").replace("<+>", "+")
-				.replace("<urseurtad>", " - 1 seurtad:").replace("<l>", "")
-				.replace("</l>", "").replace("<y>", "<span class='txy'>")
-				.replace("</y>", "</span>");
-		if (type == TYPE_MAIN) {
-			html = html.replace("<b>", "<b><i>").replace("</b>", "</i></b>")
-					.replace("<e>", "<b><i>").replace("</e>", "</i></b>");
-		} else {
-			html = html.replace("<b>", "<i>").replace("</b>", "</i>")
-					.replace("<e>", "<i>").replace("</e>", "</i>");
+		return getXName().replace("<a>", "<span class='author'>").replace(
+				"</a>", "</span>");
+	}
+
+	public String[] getNameElements() {
+		return getXName().split("</?a>");
+	}
+
+	private synchronized String getXName() {
+		if (xName == null) {
+			xName = name.replace("<x>", "×").replace("<+>", "+")
+					.replace("<urseurtad>", " - 1 seurtad:").replace("<l>", "")
+					.replace("</l>", "").replace("<y>", "<span class='txy'>")
+					.replace("</y>", "</span>");
+			if (type == TYPE_MAIN) {
+				xName = xName.replace("<b>", "<b><i>")
+						.replace("</b>", "</i></b>").replace("<e>", "<b><i>")
+						.replace("</e>", "</i></b>");
+			} else {
+				xName = xName.replace("<b>", "<i>").replace("</b>", "</i>")
+						.replace("<e>", "<i>").replace("</e>", "</i>");
+			}
+			for (String eps : EPSILON_LIST) {
+				xName = xName.replace(" " + eps + " ",
+						" <span class='epsilon'>" + eps + "</span> ");
+			}
+			for (String eps : EPSILON2_LIST) {
+				xName = xName.replace(" " + eps + " ",
+						" <span class='epsilon'>" + eps + "</span> ");
+			}
+			for (String eps : EPSILON3_LIST) {
+				xName = xName.replace(" " + eps + " ",
+						" <span class='epsilon3'>" + eps + "</span> ");
+			}
 		}
-		// TODO Author
-		// TODO epsilons
-		html = html.replace("<a>", "[").replace("</a>", "]");
-		return html;
+		return xName;
 	}
 
 	public String getTextName() {
@@ -78,11 +120,10 @@ public class AkpTaxon implements Comparable<AkpTaxon> {
 					.replaceAll(
 							"^(<l><b>.*?</b>)(((?!\\s\\[=).)*?((<e>.*?</e>)|(cv\\.)).*)",
 							"\\1@\\2");
-			// TODO EPSILON
-			// for i in range(len(EPSILONS)):
-			// taxon = re.sub(r"\s(" + EPSILONS[i].replace('.', '\.').replace(
-			// '[', '\[').replace(']', '\]') + r")\s<e>([\w-]+)</e>",
-			// r" ~\2 \1", taxon)
+			for (String epsilon : escapedEpsilonList) {
+				sortKey = sortKey.replaceAll("\\s(" + epsilon
+						+ ")\\s<e>([\\w-]+)</e>", " ~\\2 \\1");
+			}
 			sortKey = sortKey
 					.replaceAll("\\s(cv\\.)\\s'?([\\w-]+)'?", " ~\\2 \\1")
 					.replace("<x> ", "").replace("<+> ", "").replace("<x>", "")
