b954c8d90e8c1c64f36f262522570e813cf69dbd
laurent <devnull@localhost>
2012-11-24 19:26:44 +0100

    Add class tree in classification first visit page. Translate it.

--- akpwebapp/src/main/java/net/plantkelt/akp/webapp/pages/AkpFirstVisitClassificationPage.java
+++ akpwebapp/src/main/java/net/plantkelt/akp/webapp/pages/AkpFirstVisitClassificationPage.java
@@ -1,13 +1,33 @@
 package net.plantkelt.akp.webapp.pages;
 
-import org.apache.wicket.markup.html.basic.Label;
+import javax.inject.Inject;
+
+import net.plantkelt.akp.domain.AkpClass;
+import net.plantkelt.akp.service.AkpTaxonService;
+import net.plantkelt.akp.webapp.elements.AkpClassTree;
+
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
 
 public class AkpFirstVisitClassificationPage extends AkpFirstVisitPage {
 
 	private static final long serialVersionUID = 1L;
 
+	@Inject
+	private AkpTaxonService akpTaxonService;
+
 	public AkpFirstVisitClassificationPage() {
-		
-		add(new Label("classes", "TODO")); 
+
+		IModel<AkpClass> rootClassModel = new LoadableDetachableModel<AkpClass>() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected AkpClass load() {
+				return akpTaxonService.getClass(null);
+			}
+		};
+		AkpClassTree rootClassTree = new AkpClassTree("classTree",
+				rootClassModel);
+		add(rootClassTree);
 	}
 }
