71001a801b4e66de684979ec690e8bdc862c92aa
laurent gregoire <laurent.gregoire@gmail.com>
2013-05-09 19:37:49 +0200

    Allow for any language to be merged in admin db page.

--- akpwebapp/src/main/java/net/plantkelt/akp/webapp/pages/AkpAdminDbPage.java
+++ akpwebapp/src/main/java/net/plantkelt/akp/webapp/pages/AkpAdminDbPage.java
@@ -1,11 +1,20 @@
 package net.plantkelt.akp.webapp.pages;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import net.plantkelt.akp.domain.AkpLang;
 import net.plantkelt.akp.service.AkpLoginService;
 import net.plantkelt.akp.service.AkpTaxonService;
 
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
-import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.IChoiceRenderer;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 
 import com.google.inject.Inject;
 
@@ -22,13 +31,59 @@ public class AkpAdminDbPage extends AkpPageTemplate {
 	public AkpAdminDbPage() {
 
 		add(new FeedbackPanel("feedback"));
-		add(new Link<Void>("mergeLang") {
+		Form<Void> form = new Form<Void>("form");
+		add(form);
+
+		final IModel<String> lang1Model = new Model<String>();
+		final IModel<String> lang2Model = new Model<String>();
+
+		form.add(new LangDropDown("lang1", lang1Model));
+		form.add(new LangDropDown("lang2", lang2Model));
+
+		form.add(new Button("mergeLang") {
 			private static final long serialVersionUID = 1L;
 
 			@Override
-			public void onClick() {
-				akpTaxonService.mergeLang("deu", "alzasi");
-			}});
+			public void onSubmit() {
+				String lang1 = lang1Model.getObject();
+				String lang2 = lang2Model.getObject();
+				if (lang1 == null || lang2 == null) {
+					warn("Please select 2 lang to process.");
+				} else {
+					akpTaxonService.mergeLang(lang1, lang2);
+					info("Merge OK!");
+				}
+			}
+		});
 	}
 
+	private class LangDropDown extends DropDownChoice<String> {
+		private static final long serialVersionUID = 1L;
+
+		public LangDropDown(String id, final IModel<String> langXidModel) {
+			super(id, langXidModel, convertLangList(akpTaxonService
+					.getLangList()), new IChoiceRenderer<String>() {
+				private static final long serialVersionUID = 1L;
+
+				@Override
+				public Object getDisplayValue(String langXid) {
+					return langXid;
+				}
+
+				@Override
+				public String getIdValue(String langXid, int index) {
+					return langXid;
+				}
+			});
+		}
+
+	}
+
+	private static List<String> convertLangList(List<AkpLang> langList) {
+		List<String> retval = new ArrayList<String>(langList.size());
+		for (AkpLang lang : langList) {
+			retval.add(lang.getXid());
+		}
+		return retval;
+	}
 }
