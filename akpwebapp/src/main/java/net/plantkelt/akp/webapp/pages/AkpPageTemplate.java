014decadd9e46784badc1c94517faa98680a643b
laurent gregoire <laurent.gregoire@gmail.com>
2013-05-11 16:43:38 +0200

    Add Google analytics javascript tracking code in page headers.

--- akpwebapp/src/main/java/net/plantkelt/akp/webapp/pages/AkpPageTemplate.java
+++ akpwebapp/src/main/java/net/plantkelt/akp/webapp/pages/AkpPageTemplate.java
@@ -1,12 +1,16 @@
 package net.plantkelt.akp.webapp.pages;
 
+import javax.inject.Inject;
+
 import net.plantkelt.akp.domain.AkpUser;
+import net.plantkelt.akp.service.AkpLoginService;
 import net.plantkelt.akp.webapp.elements.AkpHeaderPanel;
 import net.plantkelt.akp.webapp.wicket.AkpWicketSession;
 
 import org.apache.wicket.devutils.debugbar.DebugBar;
 import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
@@ -15,6 +19,9 @@ import org.apache.wicket.request.resource.CssResourceReference;
 public class AkpPageTemplate extends WebPage {
 
 	private static final long serialVersionUID = 1L;
+
+	@Inject
+	AkpLoginService akpLoginService;
 
 	public AkpPageTemplate() {
 		super();
@@ -42,6 +49,13 @@ public class AkpPageTemplate extends WebPage {
 		super.renderHead(response);
 		response.render(CssHeaderItem.forReference(new CssResourceReference(
 				AkpPageTemplate.class, "res/akp.css")));
+		response.render(JavaScriptHeaderItem
+				.forUrl("http://www.google-analytics.com/ga.js"));
+		response.render(JavaScriptHeaderItem.forScript(
+				String.format(
+						"var pageTracker = _gat._getTracker('%s'); pageTracker._trackPageview();",
+						akpLoginService.getGoogleAnalyticsTrackerAccount()),
+				"googleAnalyticsTracker"));
 	}
 
 	public AkpUser getUtilisateur() {
