38d82344b5c738d14829147401917178357975aa
laurent gregoire <laurent.gregoire@gmail.com>
2013-05-12 13:20:51 +0200

    Add build date/time to akp.properties using maven resource filtering. Display this value into webapp.

--- akpwebapp/src/main/java/net/plantkelt/akp/webapp/pages/AkpFirstVisitToolsPage.java
+++ akpwebapp/src/main/java/net/plantkelt/akp/webapp/pages/AkpFirstVisitToolsPage.java
@@ -1,9 +1,36 @@
 package net.plantkelt.akp.webapp.pages;
 
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.jar.Manifest;
+
+import javax.servlet.ServletContext;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.protocol.http.WebApplication;
+
 public class AkpFirstVisitToolsPage extends AkpFirstVisitPage {
 
 	private static final long serialVersionUID = 1L;
 
 	public AkpFirstVisitToolsPage() {
+
+		String version = null;
+		try {
+			ServletContext application = WebApplication.get()
+					.getServletContext();
+			InputStream inputStream = application
+					.getResourceAsStream("/WEB-INF/classes/akp.properties");
+			if (inputStream != null) {
+				Properties props = new Properties();
+				props.load(inputStream);
+				version = props.getProperty("build.time");
+			} else {
+				version = "[akp.properties not found]";
+			}
+		} catch (Exception e) {
+			version = e.getMessage();
+		}
+		add(new Label("buildTime", version == null ? "?" : version));
 	}
 }
