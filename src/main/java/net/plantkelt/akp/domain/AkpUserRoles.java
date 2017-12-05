1f6125213796533d9c112dcd296db1d330ae6c1f
Laurent Grégoire <laurent.gregoire@gmail.com>
2017-12-05 16:04:28 +0100

    Add roles for verna lang edit.

--- src/main/java/net/plantkelt/akp/domain/AkpUserRoles.java
+++ src/main/java/net/plantkelt/akp/domain/AkpUserRoles.java
@@ -1,18 +1,37 @@
 package net.plantkelt.akp.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class AkpUserRoles {
 
 	// Not really a right, used for backward-compatibility
+	// TODO Remove this role
 	public static final String ROLE_USER = "USER";
-
 	public static final String ROLE_EDIT_TAXON = "EDIT_TAXON";
-
+	public static final String ROLE_EDIT_VERNA_PREFIX = "EDIT_VERNA_";
+	public static final String ROLE_EDIT_VERNA_ALL = "EDIT_VERNA_*";
 	public static final String ROLE_ADMIN = "ADMIN";
 
-	private static final String[] ALL_ROLES = { ROLE_USER, ROLE_EDIT_TAXON,
-			ROLE_ADMIN };
+	private static final List<String> ALL_ROLES;
 
-	public static String[] allRoles() {
+	static {
+		// Do not include ADMIN in the list
+		ALL_ROLES = Arrays.asList(ROLE_USER, ROLE_EDIT_TAXON,
+				ROLE_EDIT_VERNA_ALL);
+	}
+
+	public static List<String> allRoles() {
 		return ALL_ROLES;
+	}
+
+	public static List<String> allRoles(List<String> langXids) {
+		List<String> ret = new ArrayList<>(ALL_ROLES.size() + langXids.size());
+		ret.addAll(ALL_ROLES);
+		for (String langXid : langXids) {
+			ret.add(ROLE_EDIT_VERNA_PREFIX + langXid);
+		}
+		return ret;
 	}
 }
