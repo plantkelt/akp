cb9f57e7d5ad68262776e7be16f7038263044e2c
Laurent Grégoire <laurent.gregoire@gmail.com>
2017-12-06 11:33:57 +0100

    Remove autologin (dev) stuff.

--- src/main/java/net/plantkelt/akp/webapp/pages/AkpLoginPage.java
+++ src/main/java/net/plantkelt/akp/webapp/pages/AkpLoginPage.java
@@ -4,29 +4,21 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import net.plantkelt.akp.domain.AkpUser;
-import net.plantkelt.akp.service.AkpLoginService;
-import net.plantkelt.akp.service.AkpTaxonService;
-import net.plantkelt.akp.webapp.wicket.AkpWicketApplication;
-import net.plantkelt.akp.webapp.wicket.AkpWicketSession;
-
-import org.apache.wicket.RuntimeConfigurationType;
 import org.apache.wicket.authroles.authentication.panel.SignInPanel;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.Button;
-import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 import com.google.inject.Inject;
 
+import net.plantkelt.akp.service.AkpTaxonService;
+import net.plantkelt.akp.webapp.wicket.AkpWicketSession;
+
 public class AkpLoginPage extends AkpPageTemplate {
 
 	private static final long serialVersionUID = 1L;
 
-	@Inject
-	private AkpLoginService akpLoginService;
 	@Inject
 	private AkpTaxonService akpTaxonService;
 
@@ -41,43 +33,6 @@ public class AkpLoginPage extends AkpPageTemplate {
 		SignInPanel signInPanel = new SignInPanel("signInPanel", false);
 		signInPanel.setRememberMe(false);
 		add(signInPanel);
-
-		Form<Void> autologinForm = new Form<Void>("autologinForm");
-		autologinForm.setVisible(AkpWicketApplication.get()
-				.getConfigurationType() == RuntimeConfigurationType.DEVELOPMENT);
-		add(autologinForm);
-		Button autologinAdminButton = new Button("autologinAdminButton") {
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void onSubmit() {
-				AkpUser laurent = akpLoginService.getUser("laurent");
-				if (laurent != null) {
-					AkpWicketSession.get().autologin(laurent);
-					setResponsePage(AkpHomePage.class);
-				} else {
-					throw new RuntimeException(
-							"Laurent do not exists! Better start worrying.");
-				}
-			}
-		};
-		autologinForm.add(autologinAdminButton);
-		Button autologinUserButton = new Button("autologinUserButton") {
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void onSubmit() {
-				AkpUser laurent = akpLoginService.getUser("laurent2");
-				if (laurent != null) {
-					AkpWicketSession.get().autologin(laurent);
-					setResponsePage(AkpHomePage.class);
-				} else {
-					throw new RuntimeException(
-							"Laurent2 do not exists! Better start worrying.");
-				}
-			}
-		};
-		autologinForm.add(autologinUserButton);
 
 		// Misc info (last update)
 		Date lastUpdate = akpTaxonService.getLastUpdate();
